ab617704cf8eb6fbe0d7f8015705be11c9c292bb
Beliar24 <blackdid7@gmail.com>
2024-05-09 22:58:41 +0300

    done

--- homework/Calc.cpp
+++ homework/Calc.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -6,10 +7,16 @@ using namespace std;
 class Calculator {
 private:
     double current = 0.0;
+    vector<char> history;
 
 public:
+    static const char ADD = '+';
+    static const char SUBTRACT = '-';
+    static const char MULTIPLY = '*';
+    static const char DIVIDE = '/';
 
     Calculator& calculate(const int &a, const char &s) {
+        history.push_back(s);
         switch (s) {
             case SUBTRACT:
                  current -= a;
@@ -33,6 +40,31 @@ public:
         return *this;
     }
 
+    void calculate(const int &a, const int &b, const char &s) {
+        history.clear();
+        history.push_back(s);
+        switch (s) {
+            case SUBTRACT:
+                current = a - b;
+                break;
+            case ADD:
+                current = a + b;
+                break;
+            case MULTIPLY:
+                current = a * b;
+                break;
+            case DIVIDE:
+                if (a != 0) {
+                    current = a / b;
+                    break;
+                } else {
+                    cout << "a = 0, you can't divide";
+                }
+            default:
+                cout << "Wrong operator";
+        }
+    }
+
     [[nodiscard]] double getResult() const {
         return current;
     }
@@ -41,12 +73,17 @@ public:
         current = 0.0;
     }
 
-    static const char ADD = '+';
-    static const char SUBTRACT = '-';
-    static const char MULTIPLY = '*';
-    static const char DIVIDE = '/';
+    vector<char> getHistory() {
+        return history;
+    }
 
 };
+
+
+const char Calculator::ADD;
+const char Calculator::SUBTRACT;
+const char Calculator::MULTIPLY;
+const char Calculator::DIVIDE;
 
 
 int main() {
@@ -57,9 +94,21 @@ int main() {
 
     cout << calc.getResult() << endl;
 
+    for (char c : calc.getHistory()) {
+        cout << c << endl;
+    }
+
     calc.refresh();
 
     cout << calc.getResult() << endl;
+
+    calc.calculate(5, 5, Calculator::MULTIPLY);
+
+    cout << calc.getResult() << endl;
+
+    for (char c : calc.getHistory()) {
+        cout << c << endl;
+    }
 
     return 0;
 }
